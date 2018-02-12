896f487ebc48b9fea18db577b8416f8e5657fb88
Tyler Freedman <mail@tylerfreedman.com>
2018-02-12 09:59:10 +0000

    Added search, filled in more info, fixed a bunch of bugs

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,7 +9,7 @@ class ApplicationController < ActionController::Base
     @latest_blocks = Array.new
     blocks = @output["blocks"] - 6
     @output["blocks"].downto(blocks).each do |n|
-      block = JSON.parse(ApplicationController::namecoin_cli('getblock ' + ApplicationController::namecoin_cli('getblockhash ' + n.to_s)))
+      block = get_block(n.to_s)
       total_sent = 0
       block["tx"].each do |txid|
         transaction = get_transaction(txid)
@@ -20,7 +20,18 @@ class ApplicationController < ActionController::Base
       block["total_sent"] = total_sent
       @latest_blocks << block
     end
-    @latest_transactions = JSON.parse(ApplicationController::namecoin_cli('name_filter "^d/" 10'))
+    @latest_transactions = JSON.parse(ApplicationController::namecoin_cli('name_filter "^[i]?d/" 6'))
+    @latest_transactions.each_with_index do |tx, index|
+      @latest_transactions[index]["age"] = get_block(@latest_transactions[index]["height"].to_s)["time"]
+      details = get_transaction(@latest_transactions[index]["txid"])
+      details["vout"].each do |vout|
+        if vout["scriptPubKey"] && vout["scriptPubKey"]["nameOp"] && vout["scriptPubKey"]["nameOp"]["name"] == @latest_transactions[index]["name"]
+          @latest_transactions[index]["operation"] = vout["scriptPubKey"]["nameOp"]["op"]
+          break
+        end
+      end
+    end
+    @latest_transactions = @latest_transactions.sort_by {|k| k["age"]}.reverse
   end
 
   def contact
@@ -29,39 +40,79 @@ class ApplicationController < ActionController::Base
 
   # Dynamic routes start here
 
+  def search
+    query = params[:query]    
+    if query.downcase.start_with?('d/') || query.downcase.start_with?('id/')
+      redirect_to "/name/#{query}"
+    elsif query.length == 64
+      redirect_to "/tx/#{query}"
+    elsif query.count('g-zG-Z') > 0
+      redirect_to "/address/#{query}"
+    elsif query.to_s.count('a-fA-F') > 0
+      redirect_to "/block/#{query}"
+    elsif query.to_s.count('a-fA-F') == 0
+      redirect_to "/address/#{query}"
+    else
+      redirect_to '/404'
+    end
+  end
+
   def block
     @title = "Block #{params[:block]}"
-    if params[:block].to_s.count('a-fA-F') > 0
-      @output = ApplicationController::namecoin_cli('getblock ' + params[:block])
-    else
-      @output = ApplicationController::namecoin_cli('getblock ' + ApplicationController::namecoin_cli('getblockhash ' + params[:block]))
-    end
+    @output = get_block(params[:block])
 
-    if !@output.blank?
-      @output = JSON.parse(@output)
+    @name_operations = Array.new
+    transactions = Array.new
+    txids = @output["tx"]
+    txids.each do |txid|
+      details = get_transaction(txid)
+      details["vout"].each do |vout|
+        if vout["scriptPubKey"] && vout["scriptPubKey"]["nameOp"] && vout["scriptPubKey"]["nameOp"]["name"]
+          @name_operations << vout["scriptPubKey"]["nameOp"]
+          @name_operations[-1]["txid"] = txid
+        end
+      end
     end
+#    @name_operations = @out
+#    @latest_transactions.each_with_index do |tx, index|
+#      @latest_transactions[index]["age"] = get_block(@latest_transactions[index]["height"].to_s)["time"]
+##      details = get_transaction(@latest_transactions[index]["txid"])
+#      details["vout"].each do |vout|
+#        if vout["scriptPubKey"] && vout["scriptPubKey"]["nameOp"] && vout["scriptPubKey"]["nameOp"]["name"] == @latest_transactions[index]["name"]
+#          @latest_transactions[index]["operation"] = vout["scriptPubKey"]["nameOp"]["op"]
+#          break
+#        end
+#      end
+#    end
+#    @latest_transactions = @latest_transactions.sort_by {|k| k["age"]}.reverse
   end
 
   def transaction
     @title = "Transaction #{params[:transaction]}"
     @output = get_transaction(params[:transaction])
-    @output["height"] = JSON.parse(ApplicationController::namecoin_cli('getblock ' + @output["blockhash"]))["height"]
+    @output["height"] = get_block(@output["blockhash"])["height"]
   end
 
   def address
     #Come back to this later.
+    @title = "Address #{params[:address]}"
   end
 
   def name
     @title = "Name #{params[:name]}"
     params[:type] = params[:type].downcase
+    params[:name] = params[:name].downcase
     if params[:type] == 'd'
       @output = JSON.parse(ApplicationController::namecoin_cli('name_show d/' + params[:name]))
-      @history = JSON.parse(ApplicationController::namecoin_cli('name_history id/' + params[:name]))
+      @history = JSON.parse(ApplicationController::namecoin_cli('name_history d/' + params[:name]))
     elsif params[:type] == 'id'
       @output = JSON.parse(ApplicationController::namecoin_cli('name_show id/' + params[:name]))
       @history = JSON.parse(ApplicationController::namecoin_cli('name_history id/' + params[:name]))
     end
+    @history.each_with_index do |transaction, index|
+      @history[index]["time"] = get_block(transaction["height"].to_s)["time"]
+    end
+
     @history.reverse!
   end
 
@@ -72,5 +123,13 @@ class ApplicationController < ActionController::Base
 
   def get_transaction(txid)
     JSON.parse(ApplicationController::namecoin_cli("getrawtransaction #{txid} 1"))
+  end
+
+  def get_block(block)
+    if block.to_s.count('a-fA-F') > 0
+      JSON.parse(ApplicationController::namecoin_cli('getblock ' + block))
+    else
+      JSON.parse(ApplicationController::namecoin_cli('getblock ' + ApplicationController::namecoin_cli('getblockhash ' + block)))
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,8 @@ Rails.application.routes.draw do
   get '/block/:block' => 'application#block', :constraints => { :block => /[A-Za-z0-9]+/ }
   get '/tx/:transaction' => 'application#transaction', :constraints => { :transaction => /[A-Za-z0-9]+/ }
   get '/address/:address' => 'application#address', :constraints => { :address => /[A-Za-z0-9]+/ }
-  get '/name/:type/:name' => 'application#name', :constraints => { :name => /[A-Za-z0-9]+/ }
+  get '/name/:type/:name' => 'application#name', :constraints => { :name => /[A-Za-z0-9]+/, :type => /[id]+/ }
+  post '/search' => 'application#search', :constraints => { :query => /[A-Za-z0-9]+/ }
 
   get '/contact' => 'application#contact'
   get '/' => 'application#home'
